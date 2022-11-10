1d23b3397abec3abd6513e1feec1c63ae79c455f
andrew <andrew@jvdiamondtech.com>
2022-11-10 19:39:21 +0800

    feat: generate url model by scaffold

---
+++ app/views/urls/_url.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! url, :id, :original, :random_id, :created_at, :updated_at
+json.url url_url(url, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_11_10_113538) do
+
+  create_table "urls", force: :cascade do |t|
+    t.string "original"
+    t.string "random_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
